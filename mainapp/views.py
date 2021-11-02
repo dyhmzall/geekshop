32a92b1d0c2974f5d0efa17cee1e8f3a9ab6340c
Popravko Mikhail <dyhmzall@gmail.com>
2021-11-03 01:12:27 +0300

    Homework after lesson 2 (#2)

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,42 @@
+import datetime
+import json
+
 from django.shortcuts import render
 
 
 def main(request):
-    return render(request, "mainapp/index.html")
+    title = "главная"
+
+    #  получить данные из json-файла, пока не знаю, где лучше разместить этот функционал согласно философии django
+    with open('storage/products.json', 'r', encoding='utf-8') as f:
+        products = json.load(f)
+
+    content = {"title": title, "products": products}
+    return render(request, "mainapp/index.html", content)
 
 
 def products(request):
-    return render(request, "mainapp/products.html")
+    title = "продукты"
+
+    #  получить данные из json-файла, пока не знаю, где лучше разместить этот функционал согласно философии django
+    with open('storage/links_menu.json', 'r', encoding='utf-8') as f:
+        links_menu = json.load(f)
+
+    #  получить данные из json-файла, пока не знаю, где лучше разместить этот функционал согласно философии django
+    with open('storage/same_products.json', 'r', encoding='utf-8') as f:
+        same_products = json.load(f)
+
+    content = {"title": title, "links_menu": links_menu, "same_products": same_products}
+    return render(request, "mainapp/products.html", content)
 
 
 def contact(request):
-    return render(request, "mainapp/contact.html")
+    title = "о нас"
+    visit_date = datetime.datetime.now()
+
+    #  получить данные из json-файла, пока не знаю, где лучше разместить этот функционал согласно философии django
+    with open('storage/location.json', 'r', encoding='utf-8') as f:
+        locations = json.load(f)
+
+    content = {"title": title, "visit_date": visit_date, "locations": locations}
+    return render(request, "mainapp/contact.html", content)
