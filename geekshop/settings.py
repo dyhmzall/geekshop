1ce4ba340e4be0ec03261eaa04aaf765c557704a
dyhmzall <dyhmzall@gmail.com>
2021-11-20 17:13:00 +0300

    Используем имя приложения authapp по всему проекту

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -129,4 +129,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Set login path:
 #   https://docs.djangoproject.com/en/2.2/ref/settings/#login-url
-LOGIN_URL = "authnapp:login"
+LOGIN_URL = "authapp:login"
